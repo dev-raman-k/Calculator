ff3c7ea2050d3b512d33728c50c8c470170abcaa
Raman Kumar <raman.kumar.rk7oo7@gmail.com>
2022-12-13 18:50:05 +0530

    Normal Calculator Done

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+document.onkeydown = function (e) {
+    return false;
+}
 let screen = document.getElementById('screen');
 buttons = document.querySelectorAll('button');
 let screenValue = '';
@@ -26,37 +29,6 @@ for (item of buttons) {
         else if (buttonText == '=') {
             screen.value = eval(screenValue)
         }
-        else if (buttonText == "sin") {
-            var x = eval(screen.value);
-            x = x * Math.PI / 180;
-            screen.value = Math.sin(x);
-            screenValue = screen.value
-        }
-        else if (buttonText == "cos") {
-            var x = eval(screen.value);
-            x = x * Math.PI / 180;
-            screen.value = Math.cos(x);
-            screenValue = screen.value
-        }
-        else if (buttonText == "tan") {
-            var x = eval(screen.value);
-            x = x * Math.PI / 180;
-            screen.value = Math.tan(x);
-            screenValue = screen.value
-        }
-        else if (buttonText == "log") {
-            var x = eval(screen.value);
-            //x = x * Math.PI / 180;
-            screen.value = Math.LNx();
-            screenValue = screen.value
-        }
-        else if (buttonText == "ln") {
-            var x = eval(screen.value);
-            //x = x * Math.PI / 180;
-            screen.value = Math.LNx();
-            screenValue = screen.value
-        }
-        
         else {
             screenValue += buttonText;
             screen.value = screenValue;
